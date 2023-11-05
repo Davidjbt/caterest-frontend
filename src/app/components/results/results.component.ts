b51557f9cd660baa8a3a04283723370afb3099cb
Davidjbt <101603373+Davidjbt@users.noreply.github.com>
2023-11-05 13:26:40 +0000

    Start styling result component

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -10,12 +10,13 @@ import {ApiService} from "../../services/api.service";
 export class ResultsComponent {
 
   query: string = '';
+  results: any[] = [];
   constructor(private route: ActivatedRoute,
               private apiService: ApiService
   ) {
     this.route.queryParams.subscribe((params) => {
       this.query = params['query'];
-      console.log(this.query);
+      // console.log(this.query);
       this.searchUser();
     });
   }
@@ -23,7 +24,8 @@ export class ResultsComponent {
   searchUser() {
     this.apiService.getMatchingUsers(this.query).subscribe({
       next: response=> {
-        console.log(response);
+        this.results = response;
+        console.log(this.results);
       },
       error: error => {
         console.log(error);
@@ -31,6 +33,5 @@ export class ResultsComponent {
     })
   }
 
-
-
+  protected readonly length = length;
 }
